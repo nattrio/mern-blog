b9384aac5ddc20b33b36bf1221350e43b388c1bb
Jirapat Klaokliang <jirapat.nattrio@gmail.com>
2022-02-17 19:11:17 +0700

    Show all blogs

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,40 @@
 import NavbarComponent from "./components/NavbarComponent"
+import { useState, setState, useEffect } from "react"
+import axios from "axios"
+
 function App() {
+  const [blogs, setBlogs] = useState([])
+
+  const fetchData = () => {
+    axios
+      .get(`${process.env.REACT_APP_API}/blogs`)
+      .then((response) => {
+        setBlogs(response.data)
+      })
+      .catch((err) => alert(err))
+  }
+
+  useEffect(() => {
+    fetchData()
+  }, [])
   return (
     <div className="container p-5">
       <NavbarComponent />
-      <h1>MERN STACK</h1>
-      <a className="btn btn-primary" href="/create">
-        เขียนบทความ
-      </a>
+      <h1>บทความ</h1>
+      {blogs.map((blog, index) => (
+        <div className="row" key={index}>
+          <div
+            className="col pt-3 pb-2"
+            style={{ borderBottom: "1px solid silver" }}
+          >
+            <h2>{blog.title}</h2>
+            <p>{blog.content}</p>
+            <p className="text-muted">
+              ผู้เขียน: {blog.author}, เผยแพร่: {blog.createdAt}
+            </p>
+          </div>
+        </div>
+      ))}
     </div>
   )
 }
