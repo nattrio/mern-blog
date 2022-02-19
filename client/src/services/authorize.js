1243098c0836ff6657badf2ad07e0f6fba3f18bd
Jirapat Klaokliang <jirapat.nattrio@gmail.com>
2022-02-20 00:27:36 +0700

    retrieve session

--- client/src/services/authorize.js
+++ client/src/services/authorize.js
@@ -7,3 +7,25 @@ export const authenticate = (response, next) => {
   }
   next()
 }
+
+// ดึงข้อมูล token
+export const getToken = () => {
+  if (window !== "undefined") {
+    if (sessionStorage.getItem("token")) {
+      return JSON.parse(sessionStorage.getItem("token"))
+    } else {
+      return false
+    }
+  }
+}
+
+// ดึงข้อมูล user
+export const getUser = () => {
+  if (window !== "undefined") {
+    if (sessionStorage.getItem("user")) {
+      return JSON.parse(sessionStorage.getItem("user"))
+    } else {
+      return false
+    }
+  }
+}
