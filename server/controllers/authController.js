5c402e207677cba482c749b392fc1b7a0d1bda4a
Jirapat Klaokliang <jirapat.nattrio@gmail.com>
2022-02-19 23:40:56 +0700

    jwt token server

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,5 +1,20 @@
+const jwt = require("jsonwebtoken")
+
 exports.login = (req, res) => {
-  res.json({
-    data: "test login",
-  })
+  const { username, password } = req.body
+  if (password === process.env.PASSWORD) {
+    // login เข้าสู่ระบบ
+    const token = jwt.sign({ username }, process.env.JWT_SECRET, {
+      expiresIn: "1d",
+    })
+
+    return res.json({
+      token,
+      username,
+    })
+  } else {
+    return res.status(400).json({
+      err: "Incorrect password!",
+    })
+  }
 }
